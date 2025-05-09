45057d3733d8a02b21d3b5ef7601fe8434510ee3
José Sánchez-Gallego <gallegoj@uw.edu>
2025-05-09 13:58:58 -0700

    Add data_dir argument to run_reduction function

--- src/ccd/reduction.py
+++ src/ccd/reduction.py
@@ -5,12 +5,14 @@
 # @License: BSD 3-clause (http://www.opensource.org/licenses/BSD-3-Clause)
 
 
-def run_reduction():
+def run_reduction(data_dir):
     """This function must run the entire CCD reduction process. You can implement it
     in any way that you want but it must perform a valid reduction for the two
     science frames in the dataset using the functions that you have implemented in
     this module. Then perform aperture photometry on at least one of the science
-    frames, using apertures and sky annuli that make sense for the data.
+    frames, using apertures and sky annuli that make sense for the data. The function
+    must accept the data_dir as an argument, which is the path to the directory with
+    the raw data.
 
     No specific output is required but make sure the function prints/saves all the
     relevant information to the screen or to a file, and that any plots are saved to
--- tests/test_reduction.py
+++ tests/test_reduction.py
@@ -6,13 +6,15 @@
 
 from __future__ import annotations
 
+import pathlib
 
-def test_reduction():
+
+def test_reduction(data_dir: pathlib.Path):
     """Test the main reduction script. Just checks that it runs without errors."""
 
     from ccd.reduction import run_reduction
 
     # Run the reduction script
-    run_reduction()
+    run_reduction(str(data_dir))
 
     assert True  # To make sure the test is not skipped.
